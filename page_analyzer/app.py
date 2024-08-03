ae1ed55112daa5db5b8771fe5ee03a6c0a1696ee
Andrey M <tolkoigri@yandex.ru>
2024-08-04 00:27:53 +0300

    Добавлен шаблон с использованием Bootstrap и jsdelivr CDN (взят из демо-проекта).

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,4 +10,6 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def main():
-    return 'start!'
+    return render_template(
+        'main.html'
+    )
